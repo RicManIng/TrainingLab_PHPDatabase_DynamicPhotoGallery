44dec6448b1a51f570437498399e478e52bea6f4
Riccardo Mancinelli <riccardo.mancinelli@cherrybytes.it>
2024-12-17 16:29:32 +0100

    -- updates in structure

---
+++ head.php
@@ -0,0 +1,4 @@
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Galleria Fotografica</title>
+<link rel="stylesheet" href="resources/css/standard.min.css">
---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <?php
+        require_once 'head.php';
+    ?>
+</head>
+<body>
+    <?php
+        require_once 'header.php';
+    ?>
+</body>
+</html>
